1d57c5eaec28cd90ef5a18fce9a57133a4b4300f
hendrixaguirre <hendrixag@uamv.edu.ni>
2025-09-22 07:30:38 -0600

    Se creó la clase facultad satisfactoriametne

---
+++ Variables/Modelos/Facultad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Modelos
+{
+    public class Facultad
+    {
+
+        public String Nombre;
+        public String Codigo;
+
+        public String MostrarDatos()
+        {
+            return "Facultad: " + Nombre + " - Codigo: " + Codigo;
+        }
+    }
+}
